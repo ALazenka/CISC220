b75801141d55c334a69e3adb32decb9835493334
LilRaunchy <andrewlazenka@gmail.com>
2016-09-15 19:30:19 -0400

    changed file name, added vars

--- FahrenheitCelsiusCalculator.c
+++ FahrenheitCelsiusCalculator.c
@@ -2,3 +2,17 @@
 	This program will be able to print of a set list of temperatures,
 	converting them from fahrenheit to celsius.
 */
+#include 
+
+main()
+{
+	
+	int fahr, celsius, upper, lower, step;
+
+	lower = 0;
+
+	upper = 300;
+
+	step = 20;
+
+}
