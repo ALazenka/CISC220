538060650861cde5921be018677b6faea9e51f29
LilRaunchy <andrewlazenka@gmail.com>
2016-09-15 19:53:39 -0400

    added minor documentation to variables

--- FahrenheitCelsiusCalculator.c
+++ FahrenheitCelsiusCalculator.c
@@ -4,13 +4,18 @@
 */
 #include <stdio.h>
 
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP 20 /* step size */
+
+
 int main()
 {
 	
 	int fahr;
 
-	printf("Temprature Convertion Table - Celsius to Fahrenheit");
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+	printf("Temprature Convertion Table - Celsius to Fahrenheit\n");
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
 		printf("%3.0d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
 	}
 
